45bf76a2b9f927c872dec1fdb5d309a91735fba3
makosikimov-lab <makosikimov@gmail.com>
2026-07-20 06:25:24 +0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,13 @@
-print("Mako AI Scanner started...")
+import requests
+from config import DEXSCREENER_URL
+
+def get_pairs():
+    try:
+        r = requests.get(DEXSCREENER_URL, timeout=10)
+        r.raise_for_status()
+
+        return r.json()["pairs"]
+
+    except Exception as e:
+        print(e)
+        return []
